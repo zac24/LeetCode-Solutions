daa68df9b17f885b5a03f35fa33563919f8188b9
Prashant Dwivedi <prashant1.dwivedi@ril.com>
2019-06-07 13:07:12 +0530

    coded for negative numbers too

--- MyPlayground.playground/Pages/Reverse Integer.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Reverse Integer.xcplaygroundpage/Contents.swift
@@ -1,17 +1,18 @@
 //7. Reverse Integer
 
-let inputNum = 123
+let inputNum = -51743
 
 
 func reverse(_ x: Int) -> Int {
     var resultNum :Int = 0
-    var n = x
-    
+    var n : Int = Int()
+    n = x>=0 ? x : -x
     while n>0 {
         resultNum = resultNum*10 + n%10
         n = n/10
     }
     
+    resultNum = x>=0 ? resultNum : -resultNum
     return resultNum
 }
 
