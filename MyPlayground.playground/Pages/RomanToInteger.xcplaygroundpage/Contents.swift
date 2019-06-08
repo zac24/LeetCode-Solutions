3cef269abfeb854dafca14fa4a5b88f16a679cba
Prashant Dwivedi <prashant1.dwivedi@ril.com>
2019-06-08 10:53:40 +0530

    initiated romanToInt prolem

---
+++ MyPlayground.playground/Pages/RomanToInteger.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+
+func romanToInt(_ s: String) -> Int {
+    
+    var romanToIntDict: [String:Int] = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
+    var resultInt: Int = 0
+    
+    
+    for character in s {
+    }
+    
+    return 0
+}
+
+print(romanToInt("III"))
