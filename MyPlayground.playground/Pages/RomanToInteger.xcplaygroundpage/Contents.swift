c129f20b4582216c6fd19413ab636a476416a412
Prashant Dwivedi <prashant1.dwivedi@ril.com>
2019-06-11 17:27:40 +0530

    Roman Integer with while loop

--- MyPlayground.playground/Pages/RomanToInteger.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/RomanToInteger.xcplaygroundpage/Contents.swift
@@ -1,16 +1,40 @@
 
 func romanToInt(_ s: String) -> Int {
-    
+
     var romanToIntDict = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
     var resultInt: Int = 0
+    let characters = Array(s)
     
+//    for var i in 0..<characters.count {
+//        if i == characters.count - 1 {
+//            resultInt += romanToIntDict[String(characters[i])]!
+//        }
+//        if i < characters.count - 1 && (romanToIntDict[String(characters[i])]! > romanToIntDict[String(characters[i+1])]!) {
+//            resultInt += romanToIntDict[String(characters[i])]!
+//        }
+//        if i < characters.count - 1 && (romanToIntDict[String(characters[i])]! < romanToIntDict[String(characters[i+1])]!) {
+//            resultInt += romanToIntDict[String(characters[i+1])]! - romanToIntDict[String(characters[i])]!
+//            i = i + 1  // This i increments doesn't reflect on for loop.
+//        }
+//    }
     
-    for char in s {
-        print(type(of: char))
-        resultInt = resultInt + romanToIntDict[String(char)]!
+    var i : Int = 0;
+    
+    while i < characters.count {
+        if i == characters.count - 1 {
+            resultInt += romanToIntDict[String(characters[i])]!
+        }
+        if i < characters.count - 1 && (romanToIntDict[String(characters[i])]! > romanToIntDict[String(characters[i+1])]!) {
+            resultInt += romanToIntDict[String(characters[i])]!
+        }
+        if i < characters.count - 1 && (romanToIntDict[String(characters[i])]! < romanToIntDict[String(characters[i+1])]!) {
+            resultInt += romanToIntDict[String(characters[i+1])]! - romanToIntDict[String(characters[i])]!
+            i = i + 1
+        }
+        i += 1
     }
-    
     return resultInt
 }
 
-print(romanToInt("IV"))
+print(romanToInt("XC"))
+
