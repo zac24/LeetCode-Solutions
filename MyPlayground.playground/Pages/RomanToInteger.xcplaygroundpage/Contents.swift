25e52a5c1412be23abec1ee67f069f2cf433382b
Prashant Dwivedi <prashant1.dwivedi@ril.com>
2019-06-13 01:02:31 +0530

    added different way of solving romanInteger problem

--- MyPlayground.playground/Pages/RomanToInteger.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/RomanToInteger.xcplaygroundpage/Contents.swift
@@ -1,30 +1,23 @@
 
 func romanToInt(_ s: String) -> Int {
+    
+    if s.isEmpty{
+        return 0
+    }
 
-    var romanToIntDict = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
+    
     var resultInt: Int = 0
     let characters = Array(s)
-    
-//    for var i in 0..<characters.count {
-//        if i == characters.count - 1 {
-//            resultInt += romanToIntDict[String(characters[i])]!
-//        }
-//        if i < characters.count - 1 && (romanToIntDict[String(characters[i])]! > romanToIntDict[String(characters[i+1])]!) {
-//            resultInt += romanToIntDict[String(characters[i])]!
-//        }
-//        if i < characters.count - 1 && (romanToIntDict[String(characters[i])]! < romanToIntDict[String(characters[i+1])]!) {
-//            resultInt += romanToIntDict[String(characters[i+1])]! - romanToIntDict[String(characters[i])]!
-//            i = i + 1  // This i increments doesn't reflect on for loop.
-//        }
-//    }
-    
     var i : Int = 0;
     
+    
+ /* // ============= One way of Doing ========================//
+    var romanToIntDict = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
     while i < characters.count {
         if i == characters.count - 1 {
             resultInt += romanToIntDict[String(characters[i])]!
         }
-        if i < characters.count - 1 && (romanToIntDict[String(characters[i])]! > romanToIntDict[String(characters[i+1])]!) {
+        if i < characters.count - 1 && (romanToIntDict[String(characters[i])]! >= romanToIntDict[String(characters[i+1])]!) {
             resultInt += romanToIntDict[String(characters[i])]!
         }
         if i < characters.count - 1 && (romanToIntDict[String(characters[i])]! < romanToIntDict[String(characters[i+1])]!) {
@@ -33,8 +26,21 @@ func romanToInt(_ s: String) -> Int {
         }
         i += 1
     }
+    */
+    
+    // ============= Another way of Doing ========================//
+    var romanToIntDict : [Character:Int] = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
+    while i < characters.count - 1 {
+        let val = romanToIntDict[characters[i]]
+        let nextVal = romanToIntDict[characters[i+1]]
+        resultInt += (val! > nextVal!) ? val! : -val!
+        i += 1
+    }
+    
+    resultInt += romanToIntDict[characters.last!]!
+    
     return resultInt
 }
 
-print(romanToInt("XC"))
+print(romanToInt("XCV"))
 
